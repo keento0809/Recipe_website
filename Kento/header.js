1fd79faccc8f24a2c313751a9df61218c82b3a81
KEENTO <handball0321@gmail.com>
2022-03-16 15:43:59 -0700

    Add dropdown menu on nav bar

--- Kento/header.js
+++ Kento/header.js
@@ -5,6 +5,21 @@ const nav = document.querySelector(".nav-section");
 const navMenu = document.querySelector(".nav-menu");
 const titleInNav = document.querySelector(".title-in-nav");
 
+const dp = document.querySelector(".dp");
+const recipes = document.querySelector("#recipes");
+const dropdown = document.querySelector(".dropdown");
+
+const recipesInfo = recipes.getBoundingClientRect();
+console.log(recipes);
+
+const checkHandler = () => {
+  dropdown.classList.add("showing");
+};
+
+const leaveHandler = () => {
+  dropdown.classList.remove("showing");
+};
+
 // Hook up the event
 window.onscroll = function (e) {
   if (this.scrollY < 100) {
@@ -24,3 +39,6 @@ window.onscroll = function (e) {
   console.log(this.oldScroll > this.scrollY);
   this.oldScroll = this.scrollY;
 };
+
+dp.addEventListener("mouseover", checkHandler);
+dp.addEventListener("mouseout", leaveHandler);
