6b960efa4be0afb2577f622456e442b20e2430ba
Megumi Akama <kamayatu91@gmail.com>
2022-03-17 16:05:30 -0700

    Add registration function and JS file to megumi's page.

---
+++ recipe-megumi.js
@@ -0,0 +1,21 @@
+$(function(){
+  $(".modal-for-imoni").hide();
+  $(".register-imoni").click(function(){
+    $(".modal-for-imoni").fadeIn(1500);
+    $("body").css("background-color", "rgba(105, 105, 105, 0.8)");
+    $(".event-video").css("opacity", 0.5);
+    $(".googlemap-yamagata").css("opacity", 0.5);
+    $(".imoni-img").css("opacity", 0.5);
+    $(".curry-udon-img").css("opacity", 0.5);
+
+  });
+  $(".close-imoni-modal").click(function(){
+    $(".modal-for-imoni").fadeOut(1500);
+    $("body").css("background-color", "rgba(255, 255, 255, 1)");
+    $(".event-video").css("opacity", 1);
+    $(".googlemap-yamagata").css("opacity", 1);
+    $(".imoni-img").css("opacity", 1);
+    $(".curry-udon-img").css("opacity", 1);
+  });
+
+});
